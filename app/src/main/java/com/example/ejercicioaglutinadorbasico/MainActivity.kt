77fba093309fce9bcd458be4e71ae4649752caba
Charlie Montequi <carlos.montequi.rod@gmail.com>
2024-10-11 17:09:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejercicioaglutinadorbasico/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ejercicioaglutinadorbasico
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /** todo
+         * una interfaz con nombre ape 1 y ape 2
+         * una list view con dam daw asir
+         * boton limpiar qeu borre los tres primer campos
+         * boton enviar que muestre el texto de los campos
+         */
+
+    }
+}
